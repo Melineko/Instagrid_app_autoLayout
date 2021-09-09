b17606543f381abc4f08593ac5e0f45ce30472bc
Melissa Briere <briere.melissa@gmail.com>
2021-09-09 17:21:16 +0200

    Ajout des outlets et actions

--- Instagrid_app_autoLayout/Controller/ViewController.swift
+++ Instagrid_app_autoLayout/Controller/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //elements of the view
+    @IBOutlet weak var arrowUpSwipe: UIImageView!
+    @IBOutlet weak var bigGridSquare: UIView!
+    @IBOutlet weak var titleApp: UILabel!
+    @IBOutlet weak var arrowUp: UIImageView!
+    @IBOutlet weak var textSwipeUp: UILabel!
+    @IBOutlet weak var buttonTemplate1: UIButton!
+    @IBOutlet weak var buttonTemplate2: UIButton!
+    @IBOutlet weak var buttonTemplate3: UIButton!
+    @IBOutlet var buttonsPhoto: [UIButton]!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
 
-
+    @IBAction func makeTemplate1(_ sender: UIButton) {
+    }
+    @IBAction func makeTemplate2(_ sender: UIButton) {
+    }
+    @IBAction func makeTemplate3(_ sender: UIButton) {
+    }
+    
 }
 
